1dd663ed60e10e250996e8b9eee35f1b40fcb747
senfurkan92 <senfurkan92@gmail.com>
2022-04-04 06:53:01 -0400

    app card image added

---
+++ src/components/base/AppCardWithImage.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md
+    dark:bg-gray-800 dark:border-gray-700"
+  >
+    <router-link :to="{ name:'posts-post', params: {tech: props.tech, id: props.id}}">
+      <img class="rounded-t-lg"
+        :src="props.imgSrc"
+        :alt="props.imgAlt"/>
+    </router-link>
+    <div class="p-5">
+      <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {{props.title}}
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {{props.description}}
+      </p>
+      <router-link
+        href="#" :to="{ name:'posts-post', params: {tech: props.tech, id: props.id}}"
+        class="inline-flex items-center py-2 px-3 text-sm font-medium text-center
+        text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4
+        focus:outline-none focus:ring-blue-300 dark:bg-blue-600
+        dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Read more
+        <svg
+          class="ml-2 -mr-1 w-4 h-4"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0
+            01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  tech: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+  imgSrc: {
+    type: String,
+    required: true,
+  },
+  imgAlt: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
