34f999e9f7b7bcd54cd3db68ad024c460deb1a9c
senfurkan92 <senfurkan92@gmail.com>
2022-04-04 06:48:37 -0400

    most read posts design

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -8,7 +8,7 @@
           -- most recent posts --
         </h2>
         <div class="flex flex-col">
-          <div class="py-10" v-for="item, index in recentPosts" :key="index">
+          <div class="py-3" v-for="item, index in recentPosts" :key="index">
             <AppHorizontalCard
               :tech="item.tech"
               :id="item.id"
@@ -21,10 +21,23 @@
           </div>
         </div>
       </div>
-      <div class="lg:col-span-1 lg:mt-0 mt-10">
+      <div class="lg:col-span-1 px-5">
         <h2 class="capitalize text-2xl font-semibold text-center">
           -- most read posts --
         </h2>
+        <div class="grid lg:grid-cols-1 sm:grid-cols-2 grid-cols-1 gap-3">
+          <div v-for="item, index in readPosts" :key="index">
+            <AppCardWithImage
+              :tech="item.tech"
+              :id="item.id"
+              :imgSrc="item.imgSrc"
+              :imgAlt="item.imgAlt"
+              :title="item.title"
+              :description="item.description"
+            >
+            </AppCardWithImage>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -34,6 +47,7 @@
 import { reactive } from 'vue';
 import AppCarousel from '@/components/common/AppCarousel.vue';
 import AppHorizontalCard from '@/components/base/AppHorizontalCard.vue';
+import AppCardWithImage from '@/components/base/AppCardWithImage.vue';
 
 const carouselItems = reactive([
   {
@@ -54,7 +68,66 @@ const recentPosts = reactive([
   {
     tech: 'Vue',
     id: '2',
-    imgSrc: 'https://source.unsplash.com/random/300x400',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+]);
+
+const readPosts = reactive([
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
+    imgAlt: 'random unsplash img',
+    title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
+    description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
+  },
+  {
+    tech: 'Vue',
+    id: '2',
+    imgSrc: 'https://source.unsplash.com/random',
     imgAlt: 'random unsplash img',
     title: 'Curabitur placerat eu dolor eget interdum. Donec vestibulum nibh quis venenatis vulputate.',
     description: 'Duis sagittis libero at semper egestas. Integer metus sapien, ultrices non tellus id, viverra vehicula odio. Praesent mattis tempor efficitur. Phasellus euismod, tellus volutpat auctor venenatis, diam sapien volutpat eros, at viverra metus leo porttitor libero.',
