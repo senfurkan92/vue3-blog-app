7e0f38f00c8ef8de5f0f54691bc6fd8d3233cf7e
senfurkan92 <senfurkan92@gmail.com>
2022-04-07 07:03:33 -0400

    about us developing began

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-      About
+  <AppBanner :items="bannerItems"></AppBanner>
+  <div class="container mx-auto lg:py-10 py-5">
+    <div class="flex flex-col">
+      <div class="md:pb-10 pb-5">a</div>
+      <div class="md:pb-10 pb-5">
+        <AppTimeline></AppTimeline>
+      </div>
+      <div class="md:pb-10 pb-5">a</div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import AppBanner from '@/components/base/AppBanner.vue';
+import AppTimeline from '@/components/base/AppTimeline.vue';
+
+const bannerItems = [
+  {
+    to: '/',
+    title: 'Home',
+  },
+  {
+    to: '/about',
+    title: 'about',
+  },
+];
+</script>
